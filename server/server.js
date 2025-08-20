1f7cb60026f4d0eac9c75fcafc35d8d669b81109
talhakhan15799@gmail.com <talhakhan15799@gmail.com>
2025-08-20 15:44:24 +0530

    `Update allowed origins in server.js to include Vercel app URL`

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ await connectDB()
 await connectCloudinary()
 
 //allow multiple origin
-const allowedOrigins=['http://localhost:5174','https://greencart-six-mu.vercel.app']
+const allowedOrigins=['http://localhost:5173','https://greencart-six-mu.vercel.app']
 
 app.post('/stripe',express.raw({type: 'application/json'}),stripeWebhooks)
 
